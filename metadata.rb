c1cd54142d275e0590056ac4d108cf7bdd93d794
Fabricio Marcelo Pavano <fpavano@deviget.com>
2016-11-06 18:47:39 -0300

    add apt default recipe

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'ruby_server'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'Fabricio Pavano'
+maintainer_email 'faripavano@gmail.com'
 license 'all_rights'
 description 'Installs/Configures ruby_server'
 long_description 'Installs/Configures ruby_server'
@@ -13,3 +13,6 @@ version '0.1.0'
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
 # source_url 'https://github.com/<insert_org_here>/ruby_server' if respond_to?(:source_url)
+
+
+depends 'apt'     , '~> 5.0.0'
